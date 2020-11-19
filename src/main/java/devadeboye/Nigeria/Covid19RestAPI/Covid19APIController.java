c781f6fef7bda65ec261b51411f28e4a25ecab24
p1st1s <emmanueladeboye2017@gamil.com>
2020-11-19 10:57:32 +0100

    - Add crossOrigin support for endpoint /summary

--- src/main/java/devadeboye/Nigeria/Covid19RestAPI/Covid19APIController.java
+++ src/main/java/devadeboye/Nigeria/Covid19RestAPI/Covid19APIController.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class Covid19APIController {
         return services.getNationalDataSummary();
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/summary")
     public JSONObject summary() {
         return services.getNationalDataSummary();
